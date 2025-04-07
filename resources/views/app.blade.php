1dc6e8ebf1ecb9ccd1a11d49b3496696c29ce701
HBS-LearnProgramming <109016075+HBS-LearnProgramming@users.noreply.github.com>
2025-04-07 15:58:25 +0800

    update project name

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>KLSE Web Scraper</title>
 
         <!-- Fonts -->
         @vite(['resources/js/app.js', 'resources/css/app.css'])
